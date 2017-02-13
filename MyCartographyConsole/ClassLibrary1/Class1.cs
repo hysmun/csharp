e0a1d77fab3cee69074fc6f9428bd30d0aedd491
ante brajko <ante brajko>
2017-02-13 09:56:47 +0100

    Debut classe CarteObj

--- MyCartographyConsole/ClassLibrary1/Class1.cs
+++ MyCartographyConsole/ClassLibrary1/Class1.cs
@@ -6,19 +6,58 @@ using System.Threading.Tasks;
 
 namespace MyCartographyobjects
 {
-    public class CarteObj
+    /*
+     * Créer une classe abstraite CartoObj qui décrit tout objet cartographique. Elle doit contenir :
+ Un constructeur par défaut
+ Une méthode abstraite Draw()
+     */
+    abstract public class CarteObj
     {
+        #region VARIABLE MEMBRE
+        #endregion //VARIABLE MEMBRE
+        #region PROPRIETES
+        #endregion //PROPRIETES
+        #region CONSTRUCTEURS
+        CarteObj() { }
+        #endregion //CONSTRUCTEURS
+        #region METHODES
+        abstract public void Draw();
+        #endregion //METHODES
     }
 
     public class POI
     {
+        #region VARIABLE MEMBRE
+        #endregion //VARIABLE MEMBRE
+        #region PROPRIETES
+        #endregion //PROPRIETES
+        #region CONSTRUCTEURS
+        #endregion //CONSTRUCTEURS
+        #region METHODES
+        #endregion //METHODES
     }
 
     public class Polyline
     {
+        #region VARIABLE MEMBRE
+        #endregion //VARIABLE MEMBRE
+        #region PROPRIETES
+        #endregion //PROPRIETES
+        #region CONSTRUCTEURS
+        #endregion //CONSTRUCTEURS
+        #region METHODES
+        #endregion //METHODES
     }
 
     public class Polygon
     {
+        #region VARIABLE MEMBRE
+        #endregion //VARIABLE MEMBRE
+        #region PROPRIETES
+        #endregion //PROPRIETES
+        #region CONSTRUCTEURS
+        #endregion //CONSTRUCTEURS
+        #region METHODES
+        #endregion //METHODES
     }
 }
