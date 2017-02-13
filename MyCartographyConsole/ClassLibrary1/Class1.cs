d0092081eca7d793590125f39b782c70fcbe120f
ante brajko <ante brajko>
2017-02-13 09:47:49 +0100

    truc

---
+++ MyCartographyConsole/ClassLibrary1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCartographyobjects
+{
+    public class CarteObj
+    {
+    }
+
+    public class POI
+    {
+    }
+
+    public class Polyline
+    {
+    }
+
+    public class Polygon
+    {
+    }
+}
